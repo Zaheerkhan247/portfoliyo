ce60c85567dfe9723074b313b1d167daf2f9749f
Zaheerkhan247 <140317363+Zaheerkhan247@users.noreply.github.com>
2024-05-26 18:15:20 +0530

    Add files via upload

---
+++ src/assets/services_data.js
@@ -0,0 +1,40 @@
+// const Services_Data = [
+//     {
+//         s_no: "01",
+//         s_name: "Web Development",
+//         s_desc: "Building interactive, responsive, and visually appealing websites using modern technologies and best practices in web development."
+//     },
+//     {
+//         s_no: "02",
+//         s_name: "Front-End Design",
+//         s_desc: "Creating user-friendly designs and intuitive interfaces with HTML, CSS, React, and Tailwind CSS for a seamless user experience."
+//     },
+//     {
+//         s_no: "03",
+//         s_name: "Web Performance Optimization",
+//         s_desc: "Improving website speed and efficiency through careful coding, efficient use of resources, and modern optimization techniques for a smoother user experience."
+//     }
+// ];
+
+// export default Services_Data;
+
+const Services_Data = [
+    {
+        s_no: "01",
+        s_name: "Web Development",
+        s_desc: "Building interactive websites using modern technologies for optimal performance and user experience. I ensure the best coding practices and robust structures to create high-quality web applications."
+    },
+    {
+        s_no: "02",
+        s_name: "Front-End Design",
+        s_desc: "Crafting user-friendly, visually appealing interfaces with HTML, CSS, and JavaScript. I focus on intuitive navigation and seamless interactions for an engaging and enjoyable user experience."
+    },
+    {
+        s_no: "03",
+        s_name: "Dynamic Web Design",
+        s_desc: "Creating dynamic website designs with React and Tailwind CSS, I develop highly interactive and adaptive web applications that cater to changing user needs for an optimized experience."
+    }
+];
+
+export default Services_Data;
+
